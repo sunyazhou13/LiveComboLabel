9c179e6c305c2c6bae2620fdd8b5f0ec038a399a
sunyazhou <sunyazhou@kuaishou.com>
2018-10-29 18:10:05 +0800

    Initial commit

---
+++ LiveComboLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveComboLabel
+//
+//  Created by sunyazhou on 2018/10/29.
+//  Copyright © 2018 sunyazhou.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveComboLabel/ViewController.m
@@ -0,0 +1,66 @@
+//
+//  ViewController.m
+//  LiveComboLabel
+//
+//  Created by sunyazhou on 2018/10/29.
+//  Copyright © 2018 sunyazhou.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *comboLabel;
+@property (nonatomic, assign) NSUInteger danceCount;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.danceCount = 0;
+    
+    //这种字体比较粗大 看起来比较好看
+    self.comboLabel.font = [UIFont fontWithName:@"AvenirNext-BoldItalic" size:36];
+}
+
+// 数字跳动
+- (void)labelDanceAnimation:(NSTimeInterval)duration {
+    //透明度
+    CABasicAnimation *opacityAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    opacityAnimation.duration = 0.4 * duration;
+    opacityAnimation.fromValue = @0.f;
+    opacityAnimation.toValue = @1.f;
+    
+    //缩放
+    CAKeyframeAnimation *scaleAnimation = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnimation.duration = duration;
+    scaleAnimation.values = @[@3.f, @1.f, @1.2f, @1.f];
+    scaleAnimation.keyTimes = @[@0.f, @0.16f, @0.28f, @0.4f];
+    scaleAnimation.removedOnCompletion = NO;
+    scaleAnimation.fillMode = kCAFillModeForwards;
+    
+    CAAnimationGroup *flashGroup = [CAAnimationGroup animation];
+    flashGroup.animations = @[opacityAnimation, scaleAnimation];
+    flashGroup.duration = duration;
+    flashGroup.removedOnCompletion = NO;
+    flashGroup.fillMode = kCAFillModeForwards;
+    
+    [self.comboLabel.layer addAnimation:flashGroup forKey:@"kGiftComboAnimationKey"];
+}
+
+- (IBAction)clickAction:(UIButton *)sender {
+    self.danceCount++;
+    [self labelDanceAnimation:0.4];
+    self.comboLabel.text = [NSString stringWithFormat:@"+  %tu",self.danceCount];
+    if (self.danceCount % 10 == 0) {
+        [self dozenCountAnimation];
+    }
+}
+
+- (void)dozenCountAnimation {
+    //特效
+    NSLog(@"缝十特效");
+    // ... 添加特效代码 这里就不写了
+}
+
+@end
